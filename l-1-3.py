81516a67dbb28fc546e1af4c064e3ff5213a2b5e
Vladimir Korobov <korob74@yandex.ru>
2019-01-25 09:32:39 +0900

    за подсказку о PEP8 - спасибо, все исправил согласно PEP8

--- l-1-3.py
+++ l-1-3.py
@@ -1,22 +1,17 @@
-name=input("Ведите Ваше имя и фамилию   ")
-print("Привет",name)
-age=int(input("Ведите Ваш возраст   "))
-# print("Ваш возраст",age)
-weight=int(input("Ведите Ваш вес   "))
-# print("Ваш вес  ", weight)
-print(name , "  возраст: ", age ," вес: ", weight)
-if   (50 < weight < 120) and age < 30 :
+name = input("Ведите Ваше имя и фамилию   ")
+print ("Привет ", name)
+age = int(input("Ведите Ваш возраст   "))
+weight = int(input("Ведите Ваш вес   "))
+print (name, "  возраст: ", age, " вес: ", weight)
+if(50 < weight < 120) and age < 30:
     print(" Да Вы в хорошем состоянии !")
-elif (50 < weight < 120) and (30< age < 50):
+elif(50 < weight < 120) and (30 < age < 50):
     print(" Да Вы  красавчик!")
-elif (weight < 50 or weight > 120) and age > 40 :
+elif (weight < 50 or weight > 120) and age > 40:
     print(" Да Вы должны пройти врачебный осмотр !")
-elif (weight < 50 or weight > 120) and age > 30 :
+elif (weight < 50 or weight > 120) and age > 30:
     print(" Да Вы должны начать вести правильный образ жизни !")
-if (250 > weight > 120) :
+if (250 > weight > 120):
     print(" Да ещё Вы Жиробас !")
-if (20 < weight < 40) :
+if (20 < weight < 40):
     print(" Да ещё Вы Дистрофан !")
-    
-
-  
